4c5823dfc290584adf58c8065c827d9b94b689da
abecirovic3 <abecirovic3@email.com>
2021-05-07 18:30:24 +0200

    Created basic layout for GUI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1280, 960));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
