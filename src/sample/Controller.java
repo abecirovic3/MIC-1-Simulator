c2204cb44a54ba3bd831f0030b2f1d44db47360a
abecirovic3 <abecirovic3@email.com>
2021-05-18 18:57:33 +0200

    done some refactoring

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,19 +1,11 @@
 package sample;
 
 import backend.FileParser;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.MapValueFactory;
-import javafx.scene.layout.Region;
-import javafx.scene.text.Text;
-import javafx.util.Callback;
 
-
-import java.util.HashMap;
 import java.util.Map;
 
 public class Controller {
@@ -32,45 +24,6 @@ public class Controller {
         instrMeaning.setCellValueFactory(new MapValueFactory<>("meaning"));
         instrBinaryCode.setCellValueFactory(new MapValueFactory<>("binaryCode"));
 
-//        supportedInstructionsTable.setFixedCellSize(Region.USE_COMPUTED_SIZE);
-
-        // Enable multiline text in instrMeaning cells
-//        Callback cellFactory = new Callback() {
-//            @Override
-//            public Object call(Object param) {
-//                final TableCell cell = new TableCell() {
-//                    private Text text;
-//                    @Override
-//                    public void updateItem(Object item, boolean empty) {
-//                        super.updateItem(item, empty);
-//                        if (!isEmpty()) {
-//                            text= new Text(item.toString());
-//                            text.setWrappingWidth(180);
-//                            setGraphic(text);
-//                        }
-//                    }
-//                };
-//                return cell;
-//            }
-//        };
-//
-//        instrMeaning.setCellFactory(cellFactory);
-
         supportedInstructionsTable.getItems().addAll(fileParser.loadSupportedInstructionsTableData());
     }
-
-//    private ObservableList<Map<String, Object>> loadSupportedInstructionsTableData() {
-//        ObservableList<Map<String, Object>> items = FXCollections.<Map<String, Object>>observableArrayList();
-//
-//        Map<String, Object> rawData = new HashMap<>();
-//
-//        rawData.put("mnemonic", "LODD");
-//        rawData.put("instruction" , "Load directly");
-//        rawData.put("meaning", "ac:=m(x)");
-//        rawData.put("binaryCode" , "0000 xxxx xxxx xxxx");
-//
-//        items.add(rawData);
-//
-//        return items;
-//    }
 }
