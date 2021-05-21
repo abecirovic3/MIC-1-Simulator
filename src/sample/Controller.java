ca37b53c4521678d3aa64f40bec1897f107aeba5
abecirovic3 <abecirovic3@email.com>
2021-05-21 17:03:31 +0200

    Code Area is being displayed with line enumeration

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,10 +2,12 @@ package sample;
 
 import backend.FileParser;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
+import javafx.geometry.Insets;
+import javafx.geometry.Point2D;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.MapValueFactory;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
 
 import java.util.Map;
 
@@ -15,7 +17,9 @@ public class Controller {
     public TableColumn<Map, String> instrMeaning;
     public TableColumn<Map, String> instrBinaryCode;
     public TableView<Map> supportedInstructionsTable;
-    public TextArea microcodeTextArea;
+    //public TextArea microcodeTextArea;
+    public CodeArea codeArea;
+    public CodeArea microcodeArea;
 
     private FileParser fileParser = new FileParser();
 
@@ -28,6 +32,10 @@ public class Controller {
 
         supportedInstructionsTable.getItems().addAll(fileParser.loadSupportedInstructionsTableData());
 
-        microcodeTextArea.setText(fileParser.loadMicroCode());
+        microcodeArea.setParagraphGraphicFactory(LineNumberFactory.get(microcodeArea));
+        microcodeArea.replaceText(fileParser.loadMicroCode());
+
+        codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
+        //codeArea.setContextMenu( new DefaultContextMenu() );
     }
 }
