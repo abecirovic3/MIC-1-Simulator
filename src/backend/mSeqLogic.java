a2fa5a24a301d2a06a6bef50e18a882aa888ef6a
abecirovic3 <abecirovic3@email.com>
2021-10-23 18:11:53 +0200

    implemented mseqlogic

--- src/backend/mSeqLogic.java
+++ src/backend/mSeqLogic.java
@@ -1,5 +1,17 @@
 package backend;
 
 public class mSeqLogic {
+    private boolean output;
 
+    public boolean isOutput() {
+        return output;
+    }
+
+    public void setOutput(boolean output) {
+        this.output = output;
+    }
+
+    public void generateOutput(byte control, boolean n, boolean z) {
+        output = control == 3 || control == 1 && n || control == 2 && z;
+    }
 }
