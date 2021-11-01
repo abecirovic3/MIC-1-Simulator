24894d35b646763228535fd66c794beac38f2834
abecirovic3 <abecirovic3@email.com>
2021-11-01 16:36:02 +0100

    started writing more CPU tests

--- test/backend/CPUTest.java
+++ test/backend/CPUTest.java
@@ -29,4 +29,23 @@ class CPUTest {
         assertEquals(12, cpu.getRegisters()[1]);
     }
 
+    @Test
+    @DisplayName("LOCO and ADDD")
+    void storeConstant() {
+        CodeParser cp = new CodeParser();
+        String code = "LOCO 12\nSTOD 1024\n";
+        assertEquals("OK", cp.parseCode(code));
+
+        short[] machineCode = cp.getMachineCode();
+
+        CPU cpu = new CPU();
+        cpu.getMemory().write((short)0, machineCode[0]);
+        cpu.getMemory().write((short)1, machineCode[1]);
+        for (int i = 0; i < 16; i++)
+            cpu.runCycle();
+
+        assertEquals(12, cpu.getRegisters()[1]); // accumulator has value 12
+        assertEquals(12, cpu.getMemory().read((short)1024)); // memory should be written to
+    }
+
 }
