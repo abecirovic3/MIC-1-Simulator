67512a2e8746490c97f3b0e3d3846e7c7f51bd63
abecirovic3 <abecirovic3@email.com>
2021-11-04 12:55:59 +0100

    updated GUI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("MIC-1 Simulator");
         primaryStage.setScene(new Scene(root, 1280, 960));
         primaryStage.setResizable(false);
         primaryStage.show();
