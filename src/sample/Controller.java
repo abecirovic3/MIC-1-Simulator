684b54074509cf49a43a7281c441a9e931590cd5
abecirovic3 <abecirovic3@email.com>
2021-11-04 13:35:04 +0100

    registers are displayed to GUI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,16 @@
 package sample;
 
+import backend.CPU;
 import backend.CodeParser;
 import backend.FileParser;
+import backend.Register;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.MapValueFactory;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import org.fxmisc.richtext.CodeArea;
 import org.fxmisc.richtext.LineNumberFactory;
@@ -22,11 +25,17 @@ public class Controller {
     public TableColumn<Map, String> instrBinaryCode;
     public TableView<Map> supportedInstructionsTable;
 
+    public TableView registersTable;
+    public TableColumn regName;
+    public TableColumn regValue;
+
     //public TextArea microcodeTextArea;
     public CodeArea codeArea;
     public CodeArea microcodeArea;
 
     private final FileParser fileParser = new FileParser();
+
+    private CPU cpu = new CPU();
 
     @FXML
     public void initialize() {
@@ -59,6 +68,11 @@ public class Controller {
 
         codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
         //codeArea.setContextMenu( new DefaultContextMenu() );
+
+        // Registers Table
+        regName.setCellValueFactory(new PropertyValueFactory<Register, String>("name"));
+        regValue.setCellValueFactory(new PropertyValueFactory<Register, String>("value"));
+        registersTable.setItems(cpu.getRegisters());
     }
 
     public void runCodeAction(ActionEvent actionEvent) {
