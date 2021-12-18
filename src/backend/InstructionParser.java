b031d64df6ccbc7dbd92a95d5dba694c16e9075a
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2021-12-18 11:30:00 +0100

    Implemented some methods

--- src/backend/InstructionParser.java
+++ src/backend/InstructionParser.java
@@ -19,15 +19,51 @@ public class InstructionParser {
         return supportedInstructions;
     }
 
-    // TODO finish method
     public String getInstructionString(short bytes) {
         short opcode, operand;
         if ((bytes & 0xF000) == 0xF000) {
             opcode = (short) ((bytes & 0xFF00) >> 8);
-            // TODO operand
+            operand = (short) (bytes & 0x00FF);
+        }
+        else {
+            opcode = (short) ((bytes & 0xF000) >> 12);
+            operand = (short) (bytes & 0x0FFF);
+        }
+
+        if (instructionRequiresArgument(getMnemonic(opcode)))
+            return getMnemonic(opcode) + " " + String.valueOf(operand);
+        return getMnemonic(opcode);
+    }
+
+    public String getMnemonic(short opcode) {
+        String opcodeString = getOpcodeBytesString(opcode);
+        for (String mnemonic : supportedInstructions.keySet()) {
+            if (supportedInstructions.get(mnemonic).equals(opcodeString))
+                return mnemonic;
         }
         return null;
     }
 
-    // TODO more methods
+    public String getOpcodeBytesString(short opcode) {
+        StringBuilder result = new StringBuilder();
+        short delimiter = 4;
+        if ((opcode & 0x00F0) == 0x00F0)
+            delimiter = 8;
+        for (short i = 0; i < delimiter; i++) {
+            result.insert(0, opcode & 0x0001);
+            opcode >>= 1;
+        }
+        return result.toString();
+    }
+
+    public boolean instructionRequiresArgument(String mnemonic) {
+        return !(mnemonic.equals("PSHI") || mnemonic.equals("POPI") || mnemonic.equals("PUSH")
+                || mnemonic.equals("POP") || mnemonic.equals("RETN") || mnemonic.equals("SWAP"));
+    }
+
+    public boolean instructionIsJump(String mnemonic) {
+        return mnemonic.equalsIgnoreCase("JPOS") || mnemonic.equalsIgnoreCase("JZER")
+                || mnemonic.equalsIgnoreCase("JUMP") || mnemonic.equalsIgnoreCase("JNEG")
+                || mnemonic.equalsIgnoreCase("JNZE");
+    }
 }
