7043ff355e33c0b8f81b0ee4e734e7c23bb20465
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2021-12-27 19:39:30 +0100

    made NumericFactory methods static

--- src/backend/NumericFactory.java
+++ src/backend/NumericFactory.java
@@ -1,46 +1,44 @@
 package backend;
 
 public class NumericFactory {
-    private int radix = 10;
+    private static int radix = 10;
 
-    public short getShortValue(String value) {
+    public static short getShortValue(String value) {
         if (radix == 10)
             return getShortValue10(value);
         return getShortValue2(value);
     }
 
-    private short getShortValue2(String value) {
+    private static short getShortValue2(String value) {
         return (short) Integer.parseUnsignedInt(value, 2);
     }
 
-    private short getShortValue10(String value) {
+    private static short getShortValue10(String value) {
         return (short) Integer.parseInt(value);
     }
 
-    public String getStringValue(short value) {
+    public static String getStringValue(short value) {
         if (radix == 10)
             return getStringValue10(value);
         return getStringValue2(value);
     }
 
-    private String getStringValue10(short value) {
+    private static String getStringValue10(short value) {
         return String.valueOf(value);
     }
 
-    private String getStringValue2(short value) {
+    private static String getStringValue2(short value) {
         String res = Integer.toBinaryString(value);
         if (res.length() > 16)
             return res.substring(res.length()-16);
         return "0".repeat(16 - res.length()) + res;
     }
 
-
-
-    public int getRadix() {
+    public static int getRadix() {
         return radix;
     }
 
-    public void setRadix(int radix) {
-        this.radix = radix;
+    public static void setRadix(int radix) {
+        NumericFactory.radix = radix;
     }
 }
--- test/backend/NumericFactoryTest.java
+++ test/backend/NumericFactoryTest.java
@@ -5,61 +5,59 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumericFactoryTest {
-    NumericFactory numericFactory = new NumericFactory();
-
     @Test
     void getShortValueTest1() {
-        numericFactory.setRadix(10);
-        assertEquals(-32767, numericFactory.getShortValue("-32767"));
-        numericFactory.setRadix(2);
-        assertEquals(-32767, numericFactory.getShortValue("1000000000000001"));
+        NumericFactory.setRadix(10);
+        assertEquals(-32767, NumericFactory.getShortValue("-32767"));
+        NumericFactory.setRadix(2);
+        assertEquals(-32767, NumericFactory.getShortValue("1000000000000001"));
     }
 
     @Test
     void getShortValueTest2() {
-        numericFactory.setRadix(10);
-        assertEquals(-1, numericFactory.getShortValue("-1"));
-        numericFactory.setRadix(2);
-        assertEquals(-1, numericFactory.getShortValue("1111111111111111"));
+        NumericFactory.setRadix(10);
+        assertEquals(-1, NumericFactory.getShortValue("-1"));
+        NumericFactory.setRadix(2);
+        assertEquals(-1, NumericFactory.getShortValue("1111111111111111"));
     }
 
     @Test
     void getShortValueTest3() {
-        numericFactory.setRadix(10);
-        assertEquals(-32768, numericFactory.getShortValue("-32768"));
-        numericFactory.setRadix(2);
-        assertEquals(-32768, numericFactory.getShortValue("1000000000000000"));
+        NumericFactory.setRadix(10);
+        assertEquals(-32768, NumericFactory.getShortValue("-32768"));
+        NumericFactory.setRadix(2);
+        assertEquals(-32768, NumericFactory.getShortValue("1000000000000000"));
     }
 
     @Test
     void getStringValueTest1() {
-        numericFactory.setRadix(10);
-        assertEquals("10", numericFactory.getStringValue((short) 10));
-        numericFactory.setRadix(2);
-        assertEquals("0000000000001010", numericFactory.getStringValue((short) 10));
+        NumericFactory.setRadix(10);
+        assertEquals("10", NumericFactory.getStringValue((short) 10));
+        NumericFactory.setRadix(2);
+        assertEquals("0000000000001010", NumericFactory.getStringValue((short) 10));
     }
 
     @Test
     void getStringValueTest2() {
-        numericFactory.setRadix(10);
-        assertEquals("-10", numericFactory.getStringValue((short) -10));
-        numericFactory.setRadix(2);
-        assertEquals("1111111111110110", numericFactory.getStringValue((short) -10));
+        NumericFactory.setRadix(10);
+        assertEquals("-10", NumericFactory.getStringValue((short) -10));
+        NumericFactory.setRadix(2);
+        assertEquals("1111111111110110", NumericFactory.getStringValue((short) -10));
     }
 
     @Test
     void getStringValueTest3() {
-        numericFactory.setRadix(10);
-        assertEquals("-32767", numericFactory.getStringValue((short) -32767));
-        numericFactory.setRadix(2);
-        assertEquals("1000000000000001", numericFactory.getStringValue((short) -32767));
+        NumericFactory.setRadix(10);
+        assertEquals("-32767", NumericFactory.getStringValue((short) -32767));
+        NumericFactory.setRadix(2);
+        assertEquals("1000000000000001", NumericFactory.getStringValue((short) -32767));
     }
 
     @Test
     void getStringValueTest4() {
-        numericFactory.setRadix(10);
-        assertEquals("-1", numericFactory.getStringValue((short) -1));
-        numericFactory.setRadix(2);
-        assertEquals("1111111111111111", numericFactory.getStringValue((short) -1));
+        NumericFactory.setRadix(10);
+        assertEquals("-1", NumericFactory.getStringValue((short) -1));
+        NumericFactory.setRadix(2);
+        assertEquals("1111111111111111", NumericFactory.getStringValue((short) -1));
     }
 }
