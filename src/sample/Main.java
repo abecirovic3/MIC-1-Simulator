55ba804febb88d4e8b58ffb3b3e2b6ee1729da70
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-04-21 10:05:20 +0200

    Add application icon

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,16 +1,19 @@
 package sample;
 
 import backend.ObservableResourceFactory;
+import com.sun.javafx.tk.Toolkit;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("/img/app_icon.png")));
         ObservableResourceFactory resourceFactory = ObservableResourceFactory.getInstance();
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"), resourceFactory.getResources());
         primaryStage.setTitle("MIC-1 Simulator");
