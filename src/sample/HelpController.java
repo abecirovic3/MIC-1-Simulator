21161cb7928c29de31c3a7850e999c988894471a
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-04-22 11:44:14 +0200

    Add help content

--- src/sample/HelpController.java
+++ src/sample/HelpController.java
@@ -1,8 +1,14 @@
 package sample;
 
+import backend.ObservableResourceFactory;
 import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
 
 public class HelpController {
+
+    public TextArea textArea;
+
+    private final ObservableResourceFactory resourceFactory = ObservableResourceFactory.getInstance();
 
     public void setTextAreaAction(ActionEvent actionEvent) {
         System.out.println("OK");
@@ -10,5 +16,21 @@ public class HelpController {
 
     public void closeAction(ActionEvent actionEvent) {
         System.out.println("Close");
+    }
+
+    public void setIconsCreditsTextAction() {
+        textArea.setText(resourceFactory.getResources().getString("icon-credits"));
+    }
+
+    public void setReportBugsTextAction() {
+        textArea.setText(resourceFactory.getResources().getString("report-bugs-text"));
+    }
+
+    public void setHelpDevelopTextAction() {
+        textArea.setText(resourceFactory.getResources().getString("help-development-text"));
+    }
+
+    public void setShortcutsTextAction(ActionEvent actionEvent) {
+        textArea.setText(resourceFactory.getResources().getString("shortcuts-text"));
     }
 }
